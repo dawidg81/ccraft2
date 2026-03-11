c0ea1875ad058bcfb6370f59343eda8625eb39f2
dawidg81 <dawidgorski.m@gmail.com>
2026-03-11 16:07:25 +0100

    Finished implementing level packet sending

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 #include <winsock2.h>
 #include <string>
+#include <zlib.h>
+#include <vector>
+#include <cstring>
 
 using namespace std;
 
@@ -66,17 +69,67 @@ public:
 	}
 
 	void sendLevel(SOCKET socket, int x, int y, int z){
-		char level[x][y][z];
-		for(int i=0; i < x; i++){
-			for(int j=0; j < z; j++){
-				for(int k=0; k < y; k++){
-					level[i][j][k] = 0x00;
+		// PREPARE LEVEL DATA
+		int totalBlocks = x * y * z;
+		vector<uint8_t> levelData(4 + totalBlocks);
+
+		levelData[0] = (totalBlocks >> 24) & 0xFF;
+		levelData[1] = (totalBlocks >> 16) & 0xFF;
+		levelData[2] = (totalBlocks >> 8) & 0xFF;
+		levelData[3] = totalBlocks & 0xFF;
+
+		for(int iy=0; iy < y; iy++){
+			for(int iz=0; iz < z; iz++){
+				for(int ix=0; ix < x; ix++){
+					levelData[4 + ix + iz * x + iy * x * z] = 0x00;
 				}
 			}
 		}
 
-		char levelinitbuff = 0x02;
-		send(socket, &levelinitbuff, sizeof(levelinitbuff), 0);
+		// COMPRESS
+		uLongf compressedSize = compressBound(levelData.size()) + 18;
+		vector<uint8_t> compressed(compressedSize);
+
+		z_stream zs = {};
+		deflateInit2(&zs, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 15 + 16, 8, Z_DEFAULT_STRATEGY);
+		zs.next_in = levelData.data();
+		zs.avail_in = levelData.size();
+		zs.next_out = compressed.data();
+		zs.avail_out = compressedSize;
+
+		deflate(&zs, Z_FINISH);
+		compressedSize = zs.total_out;
+		deflateEnd(&zs);
+		compressed.resize(compressedSize);
+
+		// SEND PACKETS
+		uint8_t initPacket = 0x02;
+		send(socket, (char*)&initPacket, 1, 0);
+
+		size_t offset = 0;
+		size_t totalSize = compressed.size();
+
+		while(offset < totalSize){
+			char chunkPacket[1027] = {};
+			size_t chunkLen = min((size_t)1024, totalSize - offset);
+			uint8_t percent = (uint8_t)((offset + chunkLen) * 100 / totalSize);
+
+			chunkPacket[0] = 0x03;
+			chunkPacket[1] = (chunkLen >> 8) & 0xFF;
+			chunkPacket[2] = chunkLen & 0xFF;
+			memcpy(chunkPacket + 3, compressed.data() + offset, chunkLen);
+			chunkPacket[1026] = (char)percent;
+
+			send(socket, chunkPacket, 1027, 0);
+			offset += chunkLen;
+		}
+
+		char finalPacket[7] = {};
+		finalPacket[0] = 0x04;
+		finalPacket[1] = (x >> 8) & 0xFF; finalPacket[2] = x & 0xFF;
+		finalPacket[3] = (y >> 8) & 0xFF; finalPacket[4] = y & 0xFF;
+		finalPacket[5] = (z >> 8) & 0xFF; finalPacket[6] = z & 0xFF;
+		send(socket, finalPacket, sizeof(finalPacket), 0);
 	}
 };
 
