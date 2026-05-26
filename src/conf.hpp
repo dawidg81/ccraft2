55b2b8a15e6d5019e7b9cdaa1534f512d24f1bdc
dawidg81 <dawidgorski.m@gmail.com>
2026-05-26 15:47:03 +0200

    Added server config handler

--- src/conf.hpp
+++ src/conf.hpp
@@ -3,6 +3,23 @@
 
 const std::string VERSION = "0.12.3-preconfig";
 
-/* string confServerName = "ccraft Testing";
-string confServerMotd = "Welcome!";
-*/
+struct ServerConfig {
+    std::string serverName = "Default Server";
+    std::string serverMotd = "Welcome!";
+    int maxPlayers = 256;
+
+    std::string bindAddress = "0.0.0.0";
+    int port = 25565;
+
+    std::string heartbeatHost = "www.classicube.net";
+    std::string heartbeatPath = "/server/heartbeat/";
+    int         heartbeatPort = 80;
+    bool        heartbeatPublic = true;
+    int         heartbeatIntervalMinutes = 1;
+
+    int saveIntervalMinutes = 5;
+};
+
+extern ServerConfig gConfig;
+
+bool loadConfig(const std::string& path = "config.toml");
